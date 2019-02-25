a08ccdbab1edc84fe995b5ab6bf72dbf9d5fd557
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-25 21:49:47 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg"; 
-import {Switch,Route} from 'react-router-dom';  
+import logo from "./logo.svg";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from './components/NavBar';
-import ProductList from './components/ProductList';
-import Details from './components/details';
-import Cart from './components/cart';
-import Default from './components/default';
+import NavBar from "./components/NavBar";
+import ProductList from "./components/ProductList";
+import Details from "./components/details";
+import Cart from "./components/cart";
+import Default from "./components/default";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar/>
+        <NavBar />
         <Switch>
-          <Route exact path="/" component={ProductList}/>
-          <Route path="/details" component={Details}/>
-          <Route path="/cart" component={Cart}/>
-          <Route component={Default}/>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
         </Switch>
       </React.Fragment>
     );
