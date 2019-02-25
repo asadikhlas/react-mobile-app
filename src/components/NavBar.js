d441fc96b8958e4d6d5fbaa1810235b96c072f11
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-25 22:43:42 +0500

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import styled from 'styled-components';
+import {ButtonContainer} from './button';
 import logo from "../logo.svg";
 
 export default class NavBar extends Component {
@@ -35,23 +35,4 @@ https://www.iconfinder.com/Makoto_msk */}
   }
 }
 
-const ButtonContainer = styled.button`
-text-transform: capitalize;
-font-size: 1.4rem;
-background: transparent;
-border: 0.05rem solid var(--lightBlue);
-color: var(--lightBlue);
-border-radius: 0.5rem;
-padding: 0.2rem 0.5rem;
-cursor: pointer;
-margin: 0.2rem 0.5rem 0.2rem 0;
-transition: all 0.5s ease-in-out;
-&:hover {
-    background : var(--lightBlue);
-    color: var(--mainBlue);
-}
-&:focus {
-    outline: none;
-}
 
-`;
