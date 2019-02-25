4f7c956c6bebeabe224137ccf6eb410e005310ef
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-25 22:05:29 +0500

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
 import logo from "../logo.svg";
 
 export default class NavBar extends Component {
@@ -21,12 +22,36 @@ https://www.iconfinder.com/Makoto_msk */}
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus" />
+          <ButtonContainer>
+              <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+              </span>
+            
             my cart
-          </button>
+          </ButtonContainer>
         </Link>
       </nav>
     );
   }
 }
+
+const ButtonContainer = styled.button`
+text-transform: capitalize;
+font-size: 1.4rem;
+background: transparent;
+border: 0.05rem solid var(--lightBlue);
+color: var(--lightBlue);
+border-radius: 0.5rem;
+padding: 0.2rem 0.5rem;
+cursor: pointer;
+margin: 0.2rem 0.5rem 0.2rem 0;
+transition: all 0.5s ease-in-out;
+&:hover {
+    background : var(--lightBlue);
+    color: var(--mainBlue);
+}
+&:focus {
+    outline: none;
+}
+
+`;
