76243962263ec1c7ebf00cadb1a70328bbbc12f8
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-25 21:10:58 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <div className="row">
+          <div className="col-6">column no one</div>
           <div className="col-6">
-          column no one
+            <span>
+              <i className="fas fa-home" />
+            </span>
           </div>
-          <div className="col-6"><span><i className="fas fa-home"></i></span></div>
-
         </div>
       </div>
     );
