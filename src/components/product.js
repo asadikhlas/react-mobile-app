95150ee4523c648e09a7c425181d4c9bc0b24f83
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-26 23:00:48 +0500

    commit

--- src/components/product.js
+++ src/components/product.js
@@ -35,6 +35,9 @@ export default class Product extends Component {
           {/* card footer */}
           <div className="card-footer d-flex justify-content-between">
           <p className="align-self-center mb-0">{title}</p>
+          <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span>
+          </h5>
 
           </div>
         </div>
