5339ef9b022fbac3fea382f09778f4d43b9974a6
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-27 21:33:10 +0500

    commit

--- src/components/product.js
+++ src/components/product.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../context";
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
   render() {
@@ -25,7 +26,7 @@ export default class Product extends Component {
             >
               {inCart ? (
                 <p className="text-capitalize mb-0" disabled>
-                  in inCart
+                  in Cart
                 </p>
               ) : (
                 <i className="fas fa-cart-plus" />
@@ -46,6 +47,16 @@ export default class Product extends Component {
   }
 }
 
+Product.propTypes = {
+  product: propTypes.shape({
+    id: propTypes.number,
+    img: propTypes.string,
+    title: propTypes.string,
+    price: propTypes.number,
+    inCart: propTypes.bool
+  }).isRequired
+}
+
 const ProductWrapper = styled.div`
   .card {
     border-color: transparent;
@@ -54,12 +65,12 @@ const ProductWrapper = styled.div`
   .card-footer {
     background: transparent;
     border-top: transparent;
-    tranition: all 1s linear;
+    transition: all 1s linear;
   }
   &:hover {
     .card {
-      border: 0.4rem solid rgba(0, 0, 0, 0.2);
-      box-shaddow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+      border: 0.4rem solid rgba(0,0,0,0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
     }
     .card-footer {
       background: rgba(247, 247, 247);
