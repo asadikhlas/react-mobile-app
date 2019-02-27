a962828d554cd1166db663c893ffdd15a20539e9
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-27 21:19:50 +0500

    commit

--- src/components/product.js
+++ src/components/product.js
@@ -47,44 +47,52 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
-.card{
-  border-color: transparent;
-  transition: all 1s linear;
-}
-.card-footer {
-  background: transparent;
-  border-top: transparent;
-  tranition: all 1s linear;
-}
-&:hover{
-  .card{
-    border: 0.4rem solid rgba(0,0,0,0.2);
-    box-shaddow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+  .card {
+    border-color: transparent;
+    transition: all 1s linear;
   }
-  .card-footer{
-    background: rgba(247, 247, 247);
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    tranition: all 1s linear;
   }
-}
-.img-container{
-  position: relative;
-  overflow: hidden;
-}
-.card-img-top {
-  transition: all 1s linear;
-}
-.img-container:hover .card-img-top{
-  trasnform: scale(1.2);
-}
-.card-btn{
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  padding: 0.2rem 0.4rem;
-  background: var(--lightBlue);
-  border: none;
-  color: var(--mainWhite);
-  font-size: 1.4rem;
-  border-radius: 0.5rem 0 0 0;
-  transfrom: translate(100%, 100%);
-}
+  &:hover {
+    .card {
+      border: 0.4rem solid rgba(0, 0, 0, 0.2);
+      box-shaddow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+    }
+    .card-footer {
+      background: rgba(247, 247, 247);
+    }
+  }
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+  .card-img-top {
+    transition: all 1s linear;
+  }
+  .img-container:hover .card-img-top {
+    trasnform: scale(1.2);
+  }
+  .card-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    background: var(--lightBlue);
+    border: none;
+    color: var(--mainWhite);
+    font-size: 1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transfrom: translate(100%, 100%);
+    transition: all 1s linear;
+  }
+  .img-container:hover .card-btn {
+    transform: translate(0, 0);
+  }
+  .card-btn:hover {
+    color: var(--mainBlue);
+    cusor: pointer;
+  }
 `;
