1d5c619bf649b0b71f2769165f677e69bab8aea5
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-27 21:00:48 +0500

    commmit

--- src/components/product.js
+++ src/components/product.js
@@ -46,4 +46,19 @@ export default class Product extends Component {
   }
 }
 
-const ProductWrapper = styled.div``;
+const ProductWrapper = styled.div`
+.card{
+  border-color: transparent;
+  transition: all 1s linear;
+}
+.card-footer {
+  background: transparent;
+  border-top: transparent;
+  tranition: all 1s linear;
+}
+&:hover{
+  .card{
+    border: 0.4rem solid rgba(0,0,0,0.2);
+  }
+}
+`;
