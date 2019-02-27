27c7840597d80c055441566d8cec2b4c8d5377d9
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-27 22:03:48 +0500

    commit

--- src/context.js
+++ src/context.js
@@ -15,6 +15,18 @@ class ProductProvider extends Component {
   addToCart = () => {
     console.log("hello from add to cart");
   };
+  tester = () => {
+    console.log('State products :', this.state.products[0].inCart)
+    console.log('Data products :', storeProducts[0].inCart);
+    const tempProducts = [...this.state.Products];
+    tempProducts[0].inCart = true
+    this.setState(()=>{
+      return {products:tempProducts}
+    },()=>{
+      console.log('State products :', this.state.products[0].inCart)
+      console.log('Data products :', storeProducts[0].inCart);
+    })
+  }
 
 
   render() {
@@ -26,6 +38,7 @@ class ProductProvider extends Component {
           addToCart: this.addToCart
         }}
       >
+      <button onClick={this.tester}>test me</button>
         {this.props.children}
       </ProductContext.Provider>
     );
