25b93da07a65769d193c61e63f0cb593006ef6d5
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-01 21:04:12 +0500

    commit

--- src/components/details.js
+++ src/components/details.js
@@ -1,25 +1,33 @@
 import React, { Component } from "react";
-import {ProductConsumer} from '../context';
-import {Link} from 'react-router-dom';
-import {ButtonContainer} from './button';
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
+import { ButtonContainer } from "./button";
 
 export default class Details extends Component {
   render() {
     return (
       <ProductConsumer>
         {value => {
-          const {id,company,img,info,price,title,inCart} = value.detailProduct;
+          const {
+            id,
+            company,
+            img,
+            info,
+            price,
+            title,
+            inCart
+          } = value.detailProduct;
           return (
             <div className="container py-5">
-            {/* title */}
-            <div className="row">
-              <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
-                <h1>{title}</h1>
+              {/* title */}
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1>{title}</h1>
+                </div>
               </div>
+              {/* end title */}
             </div>
-            {/* end title */}
-            </div>
-          )
+          );
         }}
       </ProductConsumer>
     );
