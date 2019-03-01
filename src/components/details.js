6f4d1e8fd8ab4dc55caad56a7ed0df03f49147a3
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-01 21:33:29 +0500

    commit

--- src/components/details.js
+++ src/components/details.js
@@ -35,9 +35,7 @@ export default class Details extends Component {
                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
                   <h2>model: {title}</h2>
                   <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
-                  made by: <span className="text-uppercase">
-                  {company}
-                  </span>
+                    made by: <span className="text-uppercase">{company}</span>
                   </h4>
                   <h4 className="text-blue">
                     <strong>
@@ -46,25 +44,23 @@ export default class Details extends Component {
                     </strong>
                   </h4>
                   <p className="text-capitalize font-weight-bold mt-3 mb-0">
-                  some info about product:
+                    some info about product:
                   </p>
                   <p className="text-muted lead">{info}</p>
                   {/* button */}
                   <div>
-                     <Link to="/">
-                     <ButtonContainer>
-                       back to products
-                     </ButtonContainer>
-                     </Link>
-                     <ButtonContainer
-                     disabled={inCart? true: false}
-                     onClick={()=>{
-                       value.addToCart(id)
-                     }}
-                     >
-                     {inCart ? "inCart" : "add to cart"}
-
-                     </ButtonContainer>
+                    <Link to="/">
+                      <ButtonContainer>back to products</ButtonContainer>
+                    </Link>
+                    <ButtonContainer
+                    cart
+                      disabled={inCart ? true : false}
+                      onClick={() => {
+                        value.addToCart(id);
+                      }}
+                    >
+                      {inCart ? "inCart" : "add to cart"}
+                    </ButtonContainer>
                   </div>
                 </div>
               </div>
