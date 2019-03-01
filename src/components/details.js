6fef9df5363a4718e9ee0b7f5cc7b98a1e320e0d
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-01 21:23:23 +0500

    commit

--- src/components/details.js
+++ src/components/details.js
@@ -45,6 +45,27 @@ export default class Details extends Component {
                       {price}
                     </strong>
                   </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                  some info about product:
+                  </p>
+                  <p className="text-muted lead">{info}</p>
+                  {/* button */}
+                  <div>
+                     <Link to="/">
+                     <ButtonContainer>
+                       back to products
+                     </ButtonContainer>
+                     </Link>
+                     <ButtonContainer
+                     disabled={inCart? true: false}
+                     onClick={()=>{
+                       value.addToCart(id)
+                     }}
+                     >
+                     {inCart ? "inCart" : "add to cart"}
+
+                     </ButtonContainer>
+                  </div>
                 </div>
               </div>
             </div>
