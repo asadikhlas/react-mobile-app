1c1e320f7a26e421c8f413056ac4438db31a754a
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-01 21:57:25 +0500

    commit

--- src/context.js
+++ src/context.js
@@ -8,7 +8,9 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: []
+    cart: [],
+    modalOpen:true,
+    modalProduct:detailProduct,
   };
   componentDidMount() {
     this.setProducts();
@@ -51,6 +53,17 @@ class ProductProvider extends Component {
       }
     );
   };
+  openModal= (id) =>{
+    const product = this.getItem(id);
+    this.setState(()=>{
+      return{modalProduct:product,modalOpen:true}
+    })
+  }
+  closeModal = () => {
+    this.setState(()=>{
+      return{modalOpen:false}
+    })
+  }
 
   render() {
     return (
@@ -58,7 +71,9 @@ class ProductProvider extends Component {
         value={{
           ...this.state,
           handleDetails: this.handleDetails,
-          addToCart: this.addToCart
+          addToCart: this.addToCart,
+          openModal:this.openModal,
+          closeModal:this.closeModal
         }}
       >
         {this.props.children}
