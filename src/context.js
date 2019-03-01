9723c0c80b6602fa0b4e37997cf240695635f1ad
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-01 21:37:52 +0500

    commit

--- src/context.js
+++ src/context.js
@@ -22,9 +22,16 @@ class ProductProvider extends Component {
       return { products: tempProducts };
     });
   };
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  };
 
-  handleDetails = () => {
-    console.log("hello from details");
+  handleDetails = id => {
+    const product = this.getItem();
+    this.setState(() => {
+      return { detailProduct: product };
+    });
   };
   addToCart = id => {
     console.log(`hello from add to cart.id is ${id}`);
