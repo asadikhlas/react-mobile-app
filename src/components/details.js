fb6280604dcb185f63ea3cf32dcc098a503ca2d1
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-01 21:03:46 +0500

    commit

--- src/components/details.js
+++ src/components/details.js
@@ -8,7 +8,18 @@ export default class Details extends Component {
     return (
       <ProductConsumer>
         {value => {
-          console.log(value.detailProduct)
+          const {id,company,img,info,price,title,inCart} = value.detailProduct;
+          return (
+            <div className="container py-5">
+            {/* title */}
+            <div className="row">
+              <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                <h1>{title}</h1>
+              </div>
+            </div>
+            {/* end title */}
+            </div>
+          )
         }}
       </ProductConsumer>
     );
