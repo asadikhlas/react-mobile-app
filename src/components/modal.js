d20f2ae07b52d8ff89d569088685ece56fd9a54d
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-02 21:03:29 +0500

    commit

--- src/components/modal.js
+++ src/components/modal.js
@@ -18,8 +18,12 @@ export default class Modal extends Component {
               <ModalContainer>
                 <div className="container">
                   <div className="row">
-                    <div className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
+                    <div
+                      id="modal"
+                      className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5"
+                    >
                       <h5>item added into car</h5>
+                      <img src={img} className="img-fluid" alt="product" />
                     </div>
                   </div>
                 </div>
@@ -32,4 +36,17 @@ export default class Modal extends Component {
   }
 }
 
-const ModalContainer = styled.div``;
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  #modal {
+    background: var(--mainWhite);
+  }
+`;
