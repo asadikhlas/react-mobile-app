8315fdcb768122dbdaa2813810af0dd99577026a
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-02 21:08:08 +0500

    commit

--- src/components/modal.js
+++ src/components/modal.js
@@ -24,6 +24,18 @@ export default class Modal extends Component {
                     >
                       <h5>item added into car</h5>
                       <img src={img} className="img-fluid" alt="product" />
+                      <h5>{title}</h5>
+                      <h5 className="text-muted">price: $ {price}</h5>
+                      <Link to="/">
+                    <ButtonContainer onClick={()=>closeModal()}>
+                        store
+                    </ButtonContainer>
+                      </Link>
+                      <Link to="/cart">
+                    <ButtonContainer cart onClick={()=>closeModal()}>
+                        go to card
+                    </ButtonContainer>
+                      </Link>
                     </div>
                   </div>
                 </div>
