a0bb7bbae3e1297ddfb1cd513ca3900814c11261
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-02 21:15:26 +0500

    commit

--- src/context.js
+++ src/context.js
@@ -10,7 +10,10 @@ class ProductProvider extends Component {
     detailProduct: detailProduct,
     cart: [],
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
   componentDidMount() {
     this.setProducts();
@@ -64,6 +67,18 @@ class ProductProvider extends Component {
       return { modalOpen: false };
     });
   };
+  increment = id => {
+    console.log("this is increment method");
+  };
+  decrement = id => {
+    console.log("this is decrement method");
+  };
+  removeItem = id => {
+    console.log("item removed");
+  };
+  clearCart = () => {
+    console.log("cart was cleared");
+  };
 
   render() {
     return (
@@ -73,7 +88,11 @@ class ProductProvider extends Component {
           handleDetails: this.handleDetails,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }}
       >
         {this.props.children}
