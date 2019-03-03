1d19da2609597839a7a6a3194ca945f17067b98c
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-03 20:15:36 +0500

    commit

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export default function CartItem () {
-  return (
-    <div>
-      this is cart item
-    </div>
-  )
+export default function CartItem() {
+  return <div>this is cart item</div>;
 }
