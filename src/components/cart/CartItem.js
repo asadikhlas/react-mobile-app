d5b492201bd5997418150a2855e597fabdd3b8bd
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-03 20:21:37 +0500

    commit

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function CartItem() {
-  return <div>this is cart item</div>;
+export default function CartItem({ item, value }) {
+  const { id, title, img, price, total, count } = item;
+  const { increment, decrement, removeItem } = value;
+  return (
+    <div className="row my-1 text-captialize text-center">
+      <div className="col-10 mx-auto col-lg-2">
+        <img
+          src={img}
+          style={{
+            width: "5rem",
+            height: "5rem"
+          }}
+          className="img-fluid"
+          alt="products"
+        />
+      </div>
+    </div>
+  );
 }
