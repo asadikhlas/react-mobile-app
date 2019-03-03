773a82e2980c46ea4be0d8c947e10a347e8f8bb1
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-03 20:34:09 +0500

    commit

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -26,11 +26,11 @@ export default function CartItem({ item, value }) {
       </div>
       <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
         <div className="d-flex justify-content-center">
-        <div>
-          <span className="btn btn-black mx-1" onClick={()=>decrement(id)}> 
-
-          </span>
-        </div>
+          <div>
+            <span className="btn btn-black mx-1" onClick={() => decrement(id)}>
+              -
+            </span>
+          </div>
         </div>
       </div>
       {/*  */}
