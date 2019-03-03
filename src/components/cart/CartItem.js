21f715c866514c32e7293a5713df93ff63f3930f
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-03 20:55:30 +0500

    commit

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -4,7 +4,7 @@ export default function CartItem({ item, value }) {
   const { id, title, img, price, total, count } = item;
   const { increment, decrement, removeItem } = value;
   return (
-    <div className="row my-2 text-captialize text-center">
+    <div className="row my-3 text-captialize text-center">
       <div className="col-10 mx-auto col-lg-2">
         <img
           src={img}
